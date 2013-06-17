d48c1964e0ba072afca910d7ed384f5e9acc6aa0
Evan Sims <hello@evansims.com>
2013-06-17 13:35:09 -0500

    Add r.js build script for minified version

---
+++ build/build.js
@@ -0,0 +1,14 @@
+({
+    baseUrl: "../src",
+    name: "uximage",
+    out: "../src/uximage.min.js",
+
+    optimize: "uglify2",
+    generateSourceMaps: false,
+
+    preserveLicenseComments: true,
+    removeCombined: false,
+    skipModuleInsertion: true,
+    useStrict: true,
+    skipDirOptimize: true
+})
